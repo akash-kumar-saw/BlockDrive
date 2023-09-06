6122dbd9864409bc095cf0d9ee32785571413987
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2023-09-07 02:20:22 +0530

    Added client/src/components/Access,Share.jsx

---
+++ client/src/components/Access.jsx
@@ -0,0 +1,16 @@
+function access() {
+    return (
+        <>
+        <div className="flex flex-col h-screen w-screen items-center">
+            <div className="p-5 w-full bg-gray-200">
+                <h2 className="text-2xl text-black font-bold text-center">Access Manager</h2>
+            </div>
+            <div className="p-5 border-2 border-black h-full w-full">
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default access;
---
+++ client/src/components/Share.jsx
@@ -0,0 +1,16 @@
+function share() {
+    return (
+        <>
+        <div className="flex flex-col h-screen w-screen items-center">
+            <div className="p-5 w-full bg-gray-200">
+                <h2 className="text-2xl text-black font-bold text-center">Share NFT/Image</h2>
+            </div>
+            <div className="p-5 border-2 border-black h-full w-full">
+                
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default share;
