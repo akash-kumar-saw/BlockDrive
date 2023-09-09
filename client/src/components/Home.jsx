f4487f940699724199fe1cfc542d798a5313af8e
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2023-09-10 00:02:57 +0530

    Updated client/src/components/Home,Share.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,38 @@
-function home() {
+import {useEffect, useState} from "react";
+
+const home = ({state}) => {
+
+    const [nftMetaData, setNftMetaData] = useState([]);
+
+    useEffect(()=>{
+        const {contract, address}=state;
+    
+        const Func = async()=>{
+          const content = await contract.getNFT(address);
+          setNftMetaData(content);
+          console.log(content)
+        }
+
+        contract && Func();
+        
+    },[state])
+    
     return (
         <>
         <div className="flex flex-col bg-gray-200 h-screen w-screen items-center">
             <div className="p-5 w-full ">
                 <h2 className="text-2xl text-black font-bold text-center">My Drive</h2>
             </div>
-            <div className="p-5 border-2 rounded-tl-2xl bg-white border-black h-full w-full">
-                <a href="https://google.com">
-                    <div className="p-2 rounded-xl text-center w-[150px] h-[200px] border-2 border-black bg-gray-200 shadow-lg shadow-black hover:bg-gray-400">
-                        <h3 className="font-bold">Address</h3>
-                        <img src="" className="border-2 border-black w-full h-3/4"></img>
-                        <h3 className="font-bold">Caption</h3>
+            <div className="flex flex-wrap justify-start overflow-y-auto p-5 border-2 rounded-tl-2xl bg-white border-black h-full w-full">
+                {nftMetaData.map((nft, index) => (
+                    <div className="p-2 m-2 rounded-xl text-center w-[150px] h-[200px] border-2 border-black bg-gray-200 shadow-lg shadow-black hover:bg-gray-400">
+                        <h3 className="font-bold overflow-hidden overflow-ellipsis">{nft.owner}</h3>
+                        <a href={`https://gateway.ipfs.io/ipfs/${nft.ipfsHash}`}>
+                            <img src={`https://gateway.ipfs.io/ipfs/${nft.ipfsHash}`} className="border-2 border-black w-full h-3/4"></img>
+                        </a>
+                        <h3 className="font-bold">{nft.caption}</h3>
                     </div>
-                </a>
+                ))}
             </div>
         </div>
         </>
--- client/src/components/Share.jsx
+++ client/src/components/Share.jsx
@@ -1,4 +1,22 @@
-function share() {
+import {useEffect, useState} from "react";
+
+const share = ({state}) => {
+
+    const [nftMetaData, setNftMetaData] = useState([]);
+
+    useEffect(()=>{
+        const {contract, address}=state;
+    
+        const Func = async()=>{
+          const content = await contract.getNFT(address);
+          setNftMetaData(content);
+          console.log(content)
+        }
+
+        contract && Func();
+        
+    },[state])
+
     return (
         <>
         <div className="flex flex-col bg-gray-200 h-screen w-screen items-center">
@@ -10,14 +28,16 @@ function share() {
                     <input className="border-2 border-black w-full focus:bg-gray-100 font-bold rounded-md shadow-md shadow-black h-[50px]" placeholder="Ethereum Address" />
                     <button className="bg-green-500 hover:bg-green-600 mx-5 font-bold h-[50px] w-[100px] rounded-2xl shadow-lg shadow-black">Share</button>
                 </div>
-                <div className="p-5 h-full">
-                    <a href="https://google.com">
-                        <div className="p-2 rounded-xl text-center w-[150px] h-[200px] border-2 border-black bg-gray-200 shadow-lg shadow-black hover:bg-gray-400">
-                            <h3 className="font-bold">Address</h3>
-                            <img src="" className="border-2 border-black w-full h-3/4"></img>
-                            <h3 className="font-bold">Caption</h3>
+                <div className="flex flex-wrap justify-start overflow-y-auto p-5 h-full">
+                    {nftMetaData.map((nft, index) => (
+                        <div className="p-2 m-2 rounded-xl text-center w-[150px] h-[200px] border-2 border-black bg-gray-200 shadow-lg shadow-black hover:bg-gray-400">
+                            <h3 className="font-bold overflow-hidden overflow-ellipsis">{nft.owner}</h3>
+                            <a href={`https://gateway.ipfs.io/ipfs/${nft.ipfsHash}`}>
+                                <img src={`https://gateway.ipfs.io/ipfs/${nft.ipfsHash}`} className="border-2 border-black w-full h-3/4"></img>
+                            </a>
+                            <h3 className="font-bold">{nft.caption}</h3>
                         </div>
-                    </a>
+                    ))}
                 </div>
                 <button className="h-[100px] bg-green-500 hover:bg-green-600 shadow-inner font-bold shadow-black" >Get Share List</button>
             </div>
