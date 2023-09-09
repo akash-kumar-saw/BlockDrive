9ff781639bb75a7b20569d36ef39be4d3c486e63
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2023-09-10 01:16:04 +0530

    Updated client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Sidebar from './layouts/Sidebar'
@@ -5,16 +6,26 @@ import Home from './components/Home'
 import Access from './components/Access'
 import Share from './components/Share'
 
-function App() {
+const App = () => {
+
+  const [state,setState]=useState({
+    web3:null,
+    contract:null,
+    address:null
+  })
+  const saveState=(state)=>{
+    console.log(state);
+    setState(state);
+  }
 
   return (
     <Router>
-      <div className="flex">  
-      <Sidebar/>
+      <div className="flex bg-gray-200">  
+      <Sidebar saveState={saveState} state={state}/>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/Access" element={<Access/>}/>
-        <Route exact path="/Share" element={<Share/>}/>
+        <Route exact path="/" element={<Home state={state}/>}/>
+        <Route exact path="/Access" element={<Access state={state}/>}/>
+        <Route exact path="/Share" element={<Share state={state}/>}/>
       </Routes>
       </div>
     </Router>
